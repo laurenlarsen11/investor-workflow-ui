955c3dd189f4082c9dcd05ce4e608ef90e8099a6
lauren <laurenlarsen@berkeley.edu>
2025-06-18 13:45:27 +0100

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true
   }
-})
+});
+
 
